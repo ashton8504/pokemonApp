0d27d7765918c27985330498deabc5b093135b6b
ashton8504 <ashtonlittle8504@gmail.com>
2023-01-31 19:38:36 -0500

    saving progress

---
+++ script.js
@@ -0,0 +1,12 @@
+// Attempting to link up the input with pokemon API
+// https://stackoverflow.com/questions/66576042/how-do-i-get-a-user-input-and-insert-it-into-the-api-link-is-javascript
+
+async function getData() {
+  let userInput = document.getElementsByClassName(".userInput").value;
+  const url = `https://pokeapi.co/api/v2/pokemon?limit=151%27${userInput}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+}
+
+getData();
